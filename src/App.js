ef09ffbc44c3e46399750e6fc76458f35bd4bd95
Pavit <pavitpim@gmail.com>
2022-04-21 15:38:10 +0700

    button with how to custom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@mui/material";
+import { Settings, Add } from "@mui/icons-material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="text">TextJS</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Setting
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new Post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        h1. Heading
+      </Typography>
+      <Button variant="contained"  sx={{
+        backgroundColor : 'skyblue',
+        color : "#888",
+        margin : 5,
+        "&:hover" : { 
+          backgroundColor : "lightblue"
+        },
+        "&:disabled" : {
+          backgroundColor : "gray",
+          color:"white"
+        }
+      }}>My unique Button</Button>
+
+      
     </div>
   );
 }
